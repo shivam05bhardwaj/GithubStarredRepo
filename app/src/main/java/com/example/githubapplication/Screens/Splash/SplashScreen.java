10c71ab5186ecbc661b29c0cf70cc20906879dec
shivambhardwaj <sbsharma05@gmail.com>
2019-05-10 00:27:48 -0400

    Added Splash Screen

---
+++ app/src/main/java/com/example/githubapplication/Screens/Splash/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.example.githubapplication.Screens.Splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.githubapplication.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
